e3bbccccd5155aa0333affd5248589e1cb0ebadf
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-25 18:34:10 -0400

    changed whoami GET request

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors({optionsSuccessStatus: 200}));
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/api/whoami', (req, res) => {
-    const ip = (req.headers['x-forwarded-for'] || '').split(',')[0] || req.socket.remoteAddress;
+    const ip = req.headers["x-forwarded-for"];
     const reqLanguage = req.headers["accept-language"];
     const reqSoftware = req.headers["user-agent"];
     res.json({ipaddress: ip, language: reqLanguage, software: reqSoftware});
