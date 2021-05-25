c277184606315e1bb20b63364edfdc6d9265bfe0
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-25 15:20:26 -0400

    Added whoami api request

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express()
 const port = process.env.port || 3001;
 
-app.get('/', (req, res) => {
-    res.json({message: "Hello World"});
+app.use(cors({optionsSuccessStatus: 200}));
+
+app.set('trust proxy', 'loopback')
+
+app.get('/api/whoami', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const reqLanguage = req.headers["accept-language"];
+    const reqSoftware = req.headers["user-agent"];
+    res.json({ipaddress: ip, language: reqLanguage, software: reqSoftware});
 });
 
 app.listen(port, () => {
